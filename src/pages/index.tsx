210fb0f6319fcb0a9754e7035f74b3b4027ec7e3
Alexandr <theobroma222@gmail.com>
2021-05-03 12:46:41 +0300

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AppTitle</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Main</main>
+      <footer>footer</footer>
+    </div>
+  );
+}
